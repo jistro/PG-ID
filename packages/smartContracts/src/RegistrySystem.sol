a0eca429357032e7c6260957fa2b0d82a43b7107
jistro <contact@jistro.xyz>
2024-03-16 14:08:38 +0000

    roles and point system added

--- packages/smartContracts/src/RegistrySystem.sol
+++ packages/smartContracts/src/RegistrySystem.sol
@@ -2,16 +2,20 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract RegistrySystem is ERC721, Ownable {
+contract RegistrySystem is ERC721, AccessControl {
+
+    uint256 private _nextTokenId;
+
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant ATTESTER_ROLE = keccak256("ATTESTER_ROLE");
 
     string constant PGID = ".pjid";
 
     struct verificationMetadata {
-        uint256 timeStampBegin;
-        uint256 timeStampEnd;
-        bytes merkleRoot;
+        uint256 timeStamp;
+        uint256 points;
         uint256 level;
     }
 
@@ -20,28 +24,24 @@ contract RegistrySystem is ERC721, Ownable {
     mapping (address user => mapping(uint256 parts => uint256 mod) )public boddyOfProfile;
 
     mapping ( address user => verificationMetadata ) public dataVerfificationUser;
-    constructor(address initialOwner)
+    constructor(address admin)
         ERC721("MyToken", "MTK")
-        Ownable(initialOwner)
-    {}
+    {
+        _grantRole(ADMIN_ROLE, admin);
+    }
 
     function _baseURI() internal pure override returns (string memory) {
         return "test://pgid/";
     }
 
     function safeMint(
-        uint256 tokenId,
-        uint256 _timeStampBegin, 
-        uint256 _timeStampEnd,
-        bytes memory _merkleRoot,
-        uint256 _level
     ) public {
+        uint256 tokenId = _nextTokenId++;
         _safeMint(msg.sender, tokenId);
         dataVerfificationUser[msg.sender] = verificationMetadata({
-            timeStampBegin: _timeStampBegin,
-            timeStampEnd: _timeStampEnd,
-            merkleRoot: _merkleRoot,
-            level: _level
+            timeStamp: block.timestamp,
+            points: 0,
+            level: 0
         });
         
     }
@@ -51,24 +51,25 @@ contract RegistrySystem is ERC721, Ownable {
     }
 
     function setPointsData(
-        uint256 _timeStampBegin, 
-        uint256 _timeStampEnd,
-        bytes memory _merkleRoot,
-        uint256 _level
-    ) external returns (bool success) {
-            //if _merkleRoot and dataVerfificationUser.merkleRoot is diferent return a false
-            if (keccak256(_merkleRoot) != keccak256(dataVerfificationUser[msg.sender].merkleRoot)) {
-                dataVerfificationUser[msg.sender].merkleRoot = "";
-                return false;
-            }
-        dataVerfificationUser[msg.sender] = verificationMetadata({
-            timeStampBegin: _timeStampBegin,
-            timeStampEnd: _timeStampEnd,
-            merkleRoot: _merkleRoot,
-            level: _level
-        });
+        uint256 _points
+    ) external onlyRole(ATTESTER_ROLE){
 
-        return true;
+        dataVerfificationUser[msg.sender].points = _points;
+        if (_points >10 && _points < 30) {
+            dataVerfificationUser[msg.sender].level = 1;
+        } else if (_points >= 30 && _points < 75) {
+            dataVerfificationUser[msg.sender].level = 2;
+        } else if (_points >= 75 && _points < 150) {
+            dataVerfificationUser[msg.sender].level = 3;
+        } else if (_points >= 150 && _points < 300) {
+            dataVerfificationUser[msg.sender].level = 4;
+        } else if (_points >= 300) {
+            dataVerfificationUser[msg.sender].level = 5;
+        } else {
+            dataVerfificationUser[msg.sender].level = 0;
+        }
+    
+        dataVerfificationUser[msg.sender].timeStamp = block.timestamp;
     }
 
     /**
@@ -91,7 +92,27 @@ contract RegistrySystem is ERC721, Ownable {
         }
     }
 
+    function setAdmin(address _admin) public onlyRole(ADMIN_ROLE) {
+        _grantRole(ADMIN_ROLE, _admin);
+    }
+
+    function setAttester(address _attester) public onlyRole(ADMIN_ROLE) {
+        _grantRole(ATTESTER_ROLE, _attester);
+    }
+
+    function removeAdmin(address _admin) public onlyRole(ADMIN_ROLE) {
+        _revokeRole(ADMIN_ROLE, _admin);
+    }
+
+    function removeAttester(address _attester) public onlyRole(ADMIN_ROLE) {
+        _revokeRole(ATTESTER_ROLE, _attester);
+    }
+
     function getUsername(address _account) public view returns (string memory) {
         return usernameRegistry[_account];
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
