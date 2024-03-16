bf79a99a95dc90ff3564bb4f4e9ecf21a8fd0470
Another <joseluismanco37@gmail.com>
2024-03-16 19:44:33 +0000

    feat: add dynamic data

--- packages/frontend/src/components/dashboard/Stats.tsx
+++ packages/frontend/src/components/dashboard/Stats.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
+'use client';
+import { getUserData } from '@/services/alchemy.service';
+import React, { useEffect, useState } from 'react';
+import { useAccount } from 'wagmi';
+import { Address } from 'viem';
+import { DynamicWidget } from '@dynamic-labs/sdk-react-core';
 
 function Stats() {
+  const { address } = useAccount();
+  const [stats, setStats] = useState({ RPGF: 0, Donations: 0 });
+  useEffect(() => {
+    async function getData() {
+      if (!address) return;
+      const { Donations, RPGF } = await getUserData(address as Address);
+      setStats({
+        RPGF: RPGF.transfers.length,
+        Donations: Donations.transfers.length,
+      });
+    }
+    getData();
+  }, [address]);
   return (
     <div className='stats w-full flex flex-col gap-4 h-full py-8 text-xl justify-center'>
       <div className=' flex h-full  text-[#94716B] flex-col p-4 border-[#C0ABA7] border-[3.5px]'>
-	  <div className='flex flex-row items-center justify-between '>
-        <span >RPGF rewards</span>
-		<img className='w-8' src="/rgp.svg"></img>
-      </div>
-        <p className='text-[#594440] pb-5'>10</p>
+        <div className='flex flex-row items-center justify-between '>
+          <span>RPGF rewards</span>
+          <img className='w-8' src='/rgp.svg'></img>
+        </div>
+        <p className='text-[#594440] pb-5'>{stats.RPGF}</p>
         <span className=' text-[#94716B]'>Donation rewards</span>
-		<p className='text-[#594440]'>25</p>
+        <p className='text-[#594440]'>{stats.Donations}</p>
       </div>
       <button className='border-2 p-4 border-[#C0ABA7] border-[3.5px] text-[#594440] w-full'>
         Update impact
--- packages/frontend/src/services/alchemy.service.ts
+++ packages/frontend/src/services/alchemy.service.ts
@@ -4,31 +4,30 @@ import { Address } from 'viem'
 
 const config = {
   apiKey: alchemyApiKey,
-  network: Network.ETH_MAINNET
+  network: Network.ARB_MAINNET
 }
 
 const alchemy = new Alchemy(config)
-async function getUserData (user: Address) {
+export async function getUserData (user: Address) {
   const RPGF = await alchemy.core.getAssetTransfers({
     fromBlock: '0x0',
-    fromAddress: '0x0000000000000000000000000000000000000000',
+    fromAddress: '0x017FF2643E1A6d500A54e2c15f8186C87795CbBe',
     toAddress: user,
     excludeZeroValue: true,
     category: [
-      AssetTransfersCategory.INTERNAL,
-      AssetTransfersCategory.EXTERNAL,
       AssetTransfersCategory.ERC20
     ]
   })
   const Donations = await alchemy.core.getAssetTransfers({
     fromBlock: '0x0',
     fromAddress: user,
-    toAddress: '0x0000000000000000000000000000000000000000',
+    toAddress: '0x5E15DBf75d3819Dd9DA31Fc159Ce5bc5f3751AB0',
     excludeZeroValue: true,
     category: [
-      AssetTransfersCategory.INTERNAL,
-      AssetTransfersCategory.EXTERNAL,
       AssetTransfersCategory.ERC20
     ]
   })
+  console.debug(RPGF, user)
+  return { RPGF, Donations }
 }
+
