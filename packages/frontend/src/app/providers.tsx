506f7125ed5e75d15f84ea778960a5b1aa92017c
Another <joseluismanco37@gmail.com>
2024-03-16 18:33:16 +0000

    feat: add graphql and eas sdk

--- packages/frontend/src/app/providers.tsx
+++ packages/frontend/src/app/providers.tsx
@@ -9,32 +9,44 @@ import { DynamicWagmiConnector } from '@dynamic-labs/wagmi-connector';
 import { createConfig, WagmiProvider } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { http } from 'viem';
-import { celo } from 'viem/chains';
+import { sepolia, arbitrum } from 'viem/chains';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from '@apollo/client';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const config = createConfig({
-    chains: [celo],
+    chains: [sepolia, arbitrum],
     multiInjectedProviderDiscovery: false,
     transports: {
-      [celo.id]: http(),
+      [sepolia.id]: http(),
+      [arbitrum.id]: http(),
     },
   });
 
   const queryClient = new QueryClient();
-
+  const client = new ApolloClient({
+    uri: 'https://sepolia.easscan.org/graphql',
+    cache: new InMemoryCache(),
+  });
   return (
-    <DynamicContextProvider
-      settings={{
-        environmentId: '425efe30-9927-45e7-8df6-87767fef70ef',
-        walletConnectors: [EthereumWalletConnectors],
-      }}
-    >
-      <WagmiProvider config={config}>
-        <QueryClientProvider client={queryClient}>
-          <DynamicWagmiConnector>{children}</DynamicWagmiConnector>
-        </QueryClientProvider>
-      </WagmiProvider>
-    </DynamicContextProvider>
+    <ApolloProvider client={client}>
+      <DynamicContextProvider
+        settings={{
+          environmentId: '425efe30-9927-45e7-8df6-87767fef70ef',
+          walletConnectors: [EthereumWalletConnectors],
+        }}
+      >
+        <WagmiProvider config={config}>
+          <QueryClientProvider client={queryClient}>
+            <DynamicWagmiConnector>{children}</DynamicWagmiConnector>
+          </QueryClientProvider>
+        </WagmiProvider>
+      </DynamicContextProvider>
+    </ApolloProvider>
   );
 }
 
