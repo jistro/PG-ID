d2dc0aabc649fcebe1800f27527111f634e9628c
Another <joseluismanco37@gmail.com>
2024-03-16 18:11:42 +0000

    feat: add services

---
+++ packages/frontend/src/services/alchemy.service.ts
@@ -0,0 +1,33 @@
+import { Alchemy, AssetTransfersCategory, Network } from 'alchemy-sdk'
+import { Address } from 'viem'
+
+const config = {
+  apiKey: 'demo',
+  network: Network.ETH_MAINNET
+}
+
+const alchemy = new Alchemy(config)
+async function getUserData (user: Address) {
+  const RPGF = await alchemy.core.getAssetTransfers({
+    fromBlock: '0x0',
+    fromAddress: '0x0000000000000000000000000000000000000000',
+    toAddress: user,
+    excludeZeroValue: true,
+    category: [
+      AssetTransfersCategory.INTERNAL,
+      AssetTransfersCategory.EXTERNAL,
+      AssetTransfersCategory.ERC20
+    ]
+  })
+  const Donations = await alchemy.core.getAssetTransfers({
+    fromBlock: '0x0',
+    fromAddress: user,
+    toAddress: '0x0000000000000000000000000000000000000000',
+    excludeZeroValue: true,
+    category: [
+      AssetTransfersCategory.INTERNAL,
+      AssetTransfersCategory.EXTERNAL,
+      AssetTransfersCategory.ERC20
+    ]
+  })
+}
