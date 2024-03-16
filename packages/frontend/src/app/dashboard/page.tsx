582738c39c0972e59fdb3176d50f6a132dfdb68f
dolvin17 <huertas876@gmail.com>
2024-03-16 21:22:46 +0100

    dashboard ui finished

--- packages/frontend/src/app/dashboard/page.tsx
+++ packages/frontend/src/app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 function Page() {
   return (
-    <main className=' flex justify-center items-center'>
+    <main className=' h-screen flex justify-center items-center'>
       <div className='dashboard p-6 gap-4 w-[970px] h-[870px] bg-nouns-pastel'>
         <User />
         <Stats />
